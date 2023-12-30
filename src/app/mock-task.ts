911e5e1e013a349b8d809069cbc2b22505fc29fd
Sachin Malekar <sachinmalekar96gmail.com>
2023-12-30 16:47:48 +0530

    header, button and task list created.

---
+++ src/app/mock-task.ts
@@ -0,0 +1,23 @@
+import { Task } from './Task';
+
+export const TASKS: Task[]= [
+{
+    id:1,
+    text: "Doctors Appointment",
+    day: 'Jan 6th 9:00AM',
+    remainder:true
+},
+{
+    id:2,
+    text: "Meeting at Office",
+    day: 'Jan 8th 9:45AM',
+    remainder:true
+},
+{
+    id:3,
+    text: "Shooping",
+    day: 'Jan 7th 2:00PM',
+    remainder:true
+},
+]
+
